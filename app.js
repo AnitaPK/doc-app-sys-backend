6ec93ea0f33dff3dd5c67b003163a70dae01b3d0
AnitaPK <anita.khamitkar@gmail.com>
2026-01-02 12:01:28 +0530

    up to multer success

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors())
 
 app.use('/api/user', userRoute)
 app.use('/api/appointment', appointmentRoute)
-app.use('/uploads/', express.static(path.join(__dirname + 'uploads')))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
